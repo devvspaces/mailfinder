243d9affb4c7b83d6dfd7d2a232cb73fca3ed262
n3trob3 <netrobeweb@gmail.com>
2021-02-20 11:00:36 +0000

    'updateProductionSettings'

--- MailFinder/settings.py
+++ MailFinder/settings.py
@@ -67,10 +67,21 @@ WSGI_APPLICATION = 'MailFinder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mailfinderdb',
+        'USER': 'mailadmin',
+        'PASSWORD': 'alakio12345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
